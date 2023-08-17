7b53a22f5527f39ef04b8205e5010d1da7f461e1
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2023-08-17 15:56:34 -0700

    add importability test

---
+++ tests/test_data_utils.py
@@ -0,0 +1,6 @@
+# TODO: This module is a placeholder for eventual comprehensive pytest unit tests.
+# Currently, we just do an simple check for importability.
+# The current unit tests reside in unit_tests.ipynb in this directory.
+
+def test_data_utils_importable():
+    from climsim_utils.data_utils import data_utils
